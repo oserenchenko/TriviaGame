f0fc25b9fb4c35451f47d184b72fc1a0ce4443ba
Olga <oserenchenko@gmail.com>
2018-06-15 22:42:10 -0600

    established trivia object, trivia game object with capacity to run one round, and set timer

---
+++ assets/javascript/app.js
@@ -0,0 +1,61 @@
+var intervalId;
+
+var triviaQuestions = [{
+    "q": "What is Denver's oldest block?",
+    "aCorrect": "Larimer Square",
+    "a1": "Wynkoop",
+    "a2": "Writer Square",
+    "a3": "Platte"
+  },
+  {
+    "q": "Back in 1906, just 25 years after opening, an arch is placed in front of Denver's Union Station featuring what word(s)?",
+    "a1": "Mile High",
+    "a2": "Welcome",
+    "aCorrect": "Mizpah",
+    "a3": "Bienvenue"
+  }
+];
+
+var triviaGame = {
+  "questionIndex": 0,
+  "timer": 20,
+  "startGame": function () {
+    $("#splashScreen").hide();
+    $("#game").show();
+    triviaGame.newRound();
+  },
+  "newRound": function () {
+    $("#question").text(triviaQuestions[triviaGame.questionIndex].q);
+    var questionProperties = Object.keys(triviaQuestions[triviaGame.questionIndex]);
+    for (var i = 0; i < questionProperties.length; i++) {
+      if (questionProperties[i].startsWith("a")) {
+        var newButton = $("<button>");
+        newButton.text(triviaQuestions[triviaGame.questionIndex][questionProperties[i]]);
+        newButton.addClass("multipleChoiceAnswer");
+        $("#multipleChoice").append(newButton);
+      }
+    }
+    triviaGame.runTimer();
+  },
+  "runTimer": function () {
+    if (triviaGame.timer > 0) {
+      clearInterval(intervalId);
+      intervalId = setInterval(triviaGame.decrement, 1000);
+    }
+  },
+  "decrement": function () {
+    triviaGame.timer--;
+    $("#timer").text("Time Remaining: " + triviaGame.timer + " seconds");
+    if (triviaGame.timer === 0) {
+      stop();
+      alert("Time Up!");
+    }
+  }
+
+};
+
+
+//on click events
+$("document").ready(function () {
+  $("#start").on("click", triviaGame.startGame);
+})
